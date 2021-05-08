27ea434807dcf56304bb5a01cbe02aecab8841b3
Ben T <git@othyn.com>
2021-05-08 17:37:10 +0100

    Ground work for item re-creation

--- mods/src/main/java/com/othyn/toolsplusplus/ToolsPlusPlus.java
+++ mods/src/main/java/com/othyn/toolsplusplus/ToolsPlusPlus.java
@@ -1,33 +1,32 @@
 package com.othyn.toolsplusplus;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
+import com.othyn.toolsplusplus.core.init.ItemInit;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("toolsplusplus")
+@Mod(ToolsPlusPlus.MOD_ID)
 public class ToolsPlusPlus
 {
     // Directly reference a log4j logger.
     public static final Logger LOGGER = LogManager.getLogger();
+    
+    public static final String MOD_ID = "toolsplusplus";
 
     public ToolsPlusPlus() {
+    	IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+    	
         // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        eventBus.addListener(this::setup);
+        
+        // Register the defined items
+        ItemInit.ITEMS.register(eventBus);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
